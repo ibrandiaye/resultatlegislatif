8cc10c36eb232f2884b8a5afac7ac3756ac40a54
ibrandiaye <ibra789ndiaye@gmail.com>
2024-10-24 23:08:15 +0000

    ddd 48

--- app/Repositories/RepresentantRepository.php
+++ app/Repositories/RepresentantRepository.php
@@ -7,73 +7,73 @@ use Auth;
 use Illuminate\Support\Facades\DB;
 
 class RepresentantRepository extends RessourceRepository{
-    public function __construct(Representant $Representant){
-        $this->model = $Representant;
+    public function __construct(Representant $representant){
+        $this->model = $representant;
     }
 
     public function getByUser()
     {
-        return DB::table("Representants")
-        ->join("communes","Representants.commune_id","=","communes.id")
-        ->join("lieuvotes","Representants.lieuvote_id","=","lieuvotes.id")
+        return DB::table("representants")
+        ->join("communes","representants.commune_id","=","communes.id")
+        ->join("lieuvotes","representants.lieuvote_id","=","lieuvotes.id")
         ->join("centrevotes","lieuvotes.centrevote_id","=","centrevotes.id")
-        ->select("Representants.*","communes.nom as commune","lieuvotes.nom as lieuvote","centrevotes.nom as centrevote")
+        ->select("representants.*","communes.nom as commune","lieuvotes.nom as lieuvote","centrevotes.nom as centrevote")
         ->where("communes.arrondissement_id",Auth::user()->arrondissement_id)
         ->get();
     }
 
     public function getTel($tel)
     {
-        return DB::table("Representants")
-        ->join("communes","Representants.commune_id","=","communes.id")
-        ->join("lieuvotes","Representants.lieuvote_id","=","lieuvotes.id")
+        return DB::table("representants")
+        ->join("communes","representants.commune_id","=","communes.id")
+        ->join("lieuvotes","representants.lieuvote_id","=","lieuvotes.id")
         ->join("centrevotes","lieuvotes.centrevote_id","=","centrevotes.id")
-        ->select("Representants.*","communes.nom as commune","lieuvotes.nom as lieuvote","centrevotes.nom as centrevote")
-        ->where("Representants.tel",$tel)
+        ->select("representants.*","communes.nom as commune","lieuvotes.nom as lieuvote","centrevotes.nom as centrevote")
+        ->where("representants.tel",$tel)
         ->get();
     }
 
     public function getByLieuVote($id)
     {
-        return DB::table("Representants")
-        ->join("communes","Representants.commune_id","=","communes.id")
-        ->join("lieuvotes","Representants.lieuvote_id","=","lieuvotes.id")
+        return DB::table("representants")
+        ->join("communes","representants.commune_id","=","communes.id")
+        ->join("lieuvotes","representants.lieuvote_id","=","lieuvotes.id")
         ->join("centrevotes","lieuvotes.centrevote_id","=","centrevotes.id")
-        ->select("Representants.*","communes.nom as commune","lieuvotes.nom as lieuvote","centrevotes.nom as centrevote")
+        ->select("representants.*","communes.nom as commune","lieuvotes.nom as lieuvote","centrevotes.nom as centrevote")
         ->where("lieuvotes.id",$id)
         ->get();
     }
     public function getByLieuVoteOnly($id)
     {
-        return DB::table("Representants")
+        return DB::table("representants")
       
         ->where("lieuvote_id",$id)
         ->get();
     }
     public function getByRepresentantVote($id)
     {
-        return DB::table("Representants")
-        ->join("communes","Representants.commune_id","=","communes.id")
-        ->join("lieuvotes","Representants.lieuvote_id","=","lieuvotes.id")
+        return DB::table("representants")
+        ->join("communes","representants.commune_id","=","communes.id")
+        ->join("lieuvotes","representants.lieuvote_id","=","lieuvotes.id")
         ->join("centrevotes","lieuvotes.centrevote_id","=","centrevotes.id")
-        ->select("Representants.*","communes.nom as commune","lieuvotes.nom as lieuvote","centrevotes.nom as centrevote","communes.arrondissement_id as arrondissement_id")
-        ->where("Representants.lieuvote_id",$id)
+        ->select("representants.*","communes.nom as commune","lieuvotes.nom as lieuvote","centrevotes.nom as centrevote","communes.arrondissement_id as arrondissement_id")
+        ->where("representants.lieuvote_id",$id)
         ->get();
     }
 
     public function getByCentreVote($id)
     {
-        return DB::table("Representants")
-        ->join("communes","Representants.commune_id","=","communes.id")
-        ->join("lieuvotes","Representants.lieuvote_id","=","lieuvotes.id")
+        return DB::table("representants")
+        ->join("communes","representants.commune_id","=","communes.id")
+        ->join("lieuvotes","representants.lieuvote_id","=","lieuvotes.id")
         ->join("centrevotes","lieuvotes.centrevote_id","=","centrevotes.id")
-        ->select("Representants.*","communes.nom as commune","lieuvotes.nom as lieuvote","centrevotes.nom as centrevote")
+        ->select("representants.*","communes.nom as commune","lieuvotes.nom as lieuvote","centrevotes.nom as centrevote")
         ->where("lieuvotes.centrevote_id",$id)
         ->get();
     }
 
     public function destroyByLieuVote($id)
     {
-       return DB::table("Representants")->where("lieuvote_id",$id)->delete();
+       return DB::table("representants")->where("lieuvote_id",$id)->delete();
     }
 }
